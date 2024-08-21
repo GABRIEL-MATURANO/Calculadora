c46ea9f22442fdebb55b2a57174c7cde4b26cea0
Gabriel Maturano <gabipirata2015@gmail.com>
2024-08-21 10:15:32 -0300

    Agregar archivos de proyecto.

---
+++ Calculadora.FE/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora.FE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSumar_Click(object sender, EventArgs e)
+        {
+            int Oper1;
+            int Oper2;
+            int Resultado;
+            Oper1 =  Convert.ToInt32(txtOper1.Text);
+            Oper2 = Convert.ToInt32(txtOper2.Text);
+
+            Resultado = Oper1 + Oper2;
+
+            //txtResultado.Text = Resultado.ToString();
+            txtResultado.Text = Convert.ToString(Resultado);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Oper1=200;
+            int Oper2=150;
+            int Resultado;
+            Resultado = Oper1 * Oper2; 
+            txtResultado.Text = Resultado.ToString();
+
+        }
+    }
+}
